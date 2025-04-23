bbb14694bd569ae1be68e9c347bf2a5d09238a58
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-04-22 22:05:54 -0500

    Update Application.java

--- job-bank-rmi/common/Application.java
+++ job-bank-rmi/common/Application.java
@@ -1,1 +1,19 @@
+import java.io.Serializable;
 
+public class Application implements Serializable {
+    private String jobId;
+    private String applicantName;
+    private String email;
+
+    // Constructor
+    public Application(String jobId, String applicantName, String email) {
+        this.jobId = jobId;
+        this.applicantName = applicantName;
+        this.email = email;
+    }
+
+    // Getters
+    public String getJobId() { return jobId; }
+    public String getApplicantName() { return applicantName; }
+    public String getEmail() { return email; }
+}
