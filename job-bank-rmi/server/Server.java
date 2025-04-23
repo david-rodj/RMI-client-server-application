1353983bc83c2842eab6c558b2a2f77aa1f800d4
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-04-22 22:07:10 -0500

    Update Server.java

--- job-bank-rmi/server/Server.java
+++ job-bank-rmi/server/Server.java
@@ -1,1 +1,15 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 
+public class Server {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(1099);
+            JobService service = new JobServiceImpl();
+            registry.rebind("JobService", service);
+            System.out.println("Servidor RMI activo en puerto 1099");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
