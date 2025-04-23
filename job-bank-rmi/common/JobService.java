a083a32c62e2ddb4023ceb507511bd9e2b915be7
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-04-22 22:05:18 -0500

    Update JobService.java

--- job-bank-rmi/common/JobService.java
+++ job-bank-rmi/common/JobService.java
@@ -1,1 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
+public interface JobService extends Remote {
+    String registerJobOffer(JobOffer offer) throws RemoteException;
+    List<JobOffer> consultJobOffers() throws RemoteException;
+    String applyToJob(String applicantName, String email, String jobId) throws RemoteException;
+    List<Application> getApplications(String employerId) throws RemoteException;
+}
